09267b2a21a8fc150c97ccab804d3a7235455bde
radu <chetreanalexandru63@gmail.com>
2025-08-09 10:40:29 +0200

    I fix error.file problem

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const fs = require('node:fs');
-const http = require('node:http');
-const path = require('node:path');
+const fs = require("node:fs");
+const http = require("node:http");
+const path = require("node:path");
 
 const PORT = 8000;
 
-const STATIC_PATH = path.join(process.cwd(), './static');
+const STATIC_PATH = path.join(process.cwd(), "./static");
 
 const toBool = [() => true, () => false];
 
 const MIME_TYPES = {
-    default: 'application/octet-stream',
-    html: 'text/html; charset=UTF-8',
-    js: 'application/javascript; charset=UTF-8',
-    css: 'text/css',
-    png: 'image/png',
-    jpg: 'image/jpg',
-    gif: 'image/gif',
-    ico: 'image/x-icon',
-    svg: 'image/svg+xml'
+    default: "application/octet-stream",
+    html: "text/html; charset=UTF-8",
+    js: "application/javascript; charset=UTF-8",
+    css: "text/css",
+    png: "image/png",
+    jpg: "image/jpg",
+    gif: "image/gif",
+    ico: "image/x-icon",
+    svg: "image/svg+xml",
 };
 
 const prepareFile = async (url) => {
     const paths = [STATIC_PATH, url];
-    if (url.endsWith('/')) paths.push('index.html');
+    if (url.endsWith("/")) paths.push("index.html");
     const filePath = path.join(...paths);
     const pathTravelsal = !filePath.startsWith(STATIC_PATH);
     const exists = await fs.promises.access(filePath).then(...toBool);
     const found = !pathTravelsal && exists;
-    const streamPath = found ? filePath : STATIC_PATH + './404.html';
+    const streamPath = found ? filePath : path.join(STATIC_PATH, "404.html");
     const ext = path.extname(streamPath).substring(1).toLowerCase();
     const stream = fs.createReadStream(streamPath);
-    return {found, ext, stream};
-}
+    return { found, ext, stream };
+};
 
 http.createServer(async (req, res) => {
     const file = await prepareFile(req.url);
     const statusCode = file.found ? 200 : 404;
     const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
-    res.writeHead(statusCode, {'Content-Type': mimeType});
+    res.writeHead(statusCode, { "Content-Type": mimeType });
     file.stream.pipe(res);
     console.log(`${req.method} ${req.url} ${statusCode}`);
-}).listen(PORT);
+})
+    .listen(PORT);
 
 console.log(`Server running at http://127.0.0.1:${PORT}`);
